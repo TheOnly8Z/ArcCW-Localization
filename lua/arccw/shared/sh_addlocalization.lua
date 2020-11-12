c14b2deb4480165877cc27d3321f4793c58aba78
TheOnly8Z <888888zombies@gmail.com>
2020-11-12 06:36:05 -0500

    Separate base and CSP for zh-cn

--- lua/arccw/shared/sh_addlocalization.lua
+++ lua/arccw/shared/sh_addlocalization.lua
@@ -1,6 +1,9 @@
+-- TEMP solution: Just add your strings below the example if you want
 local function AddLocalization()
     -- Language not added to TTT yet but whatevs
     if engine.ActiveGamemode() ~= "terrortown" then return end
+
+    -- Chinese
     LANG.AddToLanguage("简体中文", "search_dmg_buckshot", "这个人被霰弹喷了一脸。")
     LANG.AddToLanguage("简体中文", "search_dmg_nervegas", "这人脸色苍白，一定是被毒气害死了。")
     LANG.AddToLanguage("简体中文", "ammo_smg1_grenade", "枪榴弹")
