541c09276c526895b5f4111ceed831bf45c23426
TheOnly8Z <8888888zombies@gmail.com>
2021-01-25 20:03:38 +0800

    LANG check

--- lua/arccw/shared/sh_addlocalization.lua
+++ lua/arccw/shared/sh_addlocalization.lua
@@ -4,9 +4,11 @@ local function AddLocalization()
     if engine.ActiveGamemode() ~= "terrortown" then return end
 
     -- Chinese
-    LANG.AddToLanguage("简体中文", "search_dmg_buckshot", "这个人被霰弹喷了一脸。")
-    LANG.AddToLanguage("简体中文", "search_dmg_nervegas", "这人脸色苍白，一定是被毒气害死了。")
-    LANG.AddToLanguage("简体中文", "ammo_smg1_grenade", "枪榴弹")
+    if CLIENT and LANG then
+        LANG.AddToLanguage("简体中文", "search_dmg_buckshot", "这个人被霰弹喷了一脸。")
+        LANG.AddToLanguage("简体中文", "search_dmg_nervegas", "这人脸色苍白，一定是被毒气害死了。")
+        LANG.AddToLanguage("简体中文", "ammo_smg1_grenade", "枪榴弹")
+    end
 end
 
 hook.Add("ArcCW_LocalizationLoaded", "ArcCW_CustomLangs", AddLocalization)
