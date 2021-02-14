a2159572df23e7d185a5edc37377e9542bb69c25
Matheus Silagi <matsilagi2@hotmail.com>
2021-02-14 20:04:59 -0300

    Added translations
    Defo not final, but should be good enough for now.

---
+++ lua/arccw/shared/languages/fas2_pt-br.lua
@@ -0,0 +1,28 @@
+L = {}
+
+L["con.fas2.m79.quad"] = "Granadas possuem dano efetivo menor"
+
+L["pro.fas2.m79.dmg"] = "+ Dano em área"
+L["pro.fas2.m79.rad"] = "+ Área de dano"
+L["con.fas2.m79.dmg"] = "- Dano em área"
+L["con.fas2.m79.rad"] = "- Área de dano"
+
+L["pro.fas2.m79.smoke"] = "Cortina de fumaça após impacto"
+L["pro.fas2.m79.flash"] = "Flashbang após impacto"
+L["pro.fas2.m79.incendiary"] = "Chamas após impacto"
+L["pro.fas2.m79.buck"] = "Atira cartuchos .12"
+L["info.fas2.m79.buck"] = "Usa munição Calibre 12 ao invés de granadas."
+L["pro.fas2.m79.hornet"] = "Atira balas"
+L["info.fas2.m79.hornet"] = "Usa munição de pistola ao invés de granadas"
+L["pro.fas2.m79.cball"] = "Atira uma esfera de energia"
+L["info.fas2.m79.cball"] = "Usa orbes de AR2 ao invés de granadas"
+L["con.fas2.m79.impactdefuse"] = "Desativa após impacto"
+L["con.fas2.m79.noimpact"] = "Não explode após impacto"
+L["info.fas2.m79.timer"] = "Mude os modos de tiro para mudar o tempo de detonação"
+L["con.fas2.m79.min"] = "Dano direto mínimo"
+
+L["pro.fas2.double_barrel"] = "Tiro extra por disparo"
+L["pro.fas2.raptor_stock"] = "-30/40/50% Recuo nos primeiros 3 tiros"
+
+L["pro.fas2.akimbo"] = "100% mais armas"
+L["con.fas2.akimbo"] = "Sem ponto de mira"
