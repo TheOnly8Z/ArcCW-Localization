84dc65dd242af2b3c169564a8e0a6c1ed20b99d4
TheOnly8Z <8888888zombies@gmail.com>
2021-05-27 21:17:23 +0800

    localize part of gso

---
+++ lua/arccw/shared/languages/go_extra_zh-cn.lua
@@ -0,0 +1,40 @@
+L = {}
+
+L["pro.gsoe.hairtrigger"] = "-60% 扳机延迟"
+L["pro.gsoe.foregrip_combat"] = "-25% 开镜时后坐力"
+L["pro.gsoe.stock_m249"] = "-30% 不开镜时后坐力"
+L["pro.gsoe.perk_headshot"] = "+50% 爆头伤害"
+L["pro.gsoe.burst"] = "点射开火模式"
+L["con.gsoe.nog18"] = "不能和 G18 上身同时使用"
+L["con.gsoe.cook"] = "过热后会点燃弹夹和用户"
+L["pro.gsoe.perk_underwater"] = "-50% 水下后坐力"
+
+L["con.gsoe.heat_slow"] = "过热提升时射速降低"
+L["pro.gsoe.heat_fast"] = "过热提升时射速增加"
+L["pro.gsoe.heat_fix"] = "修理武器后立刻解除过热"
+
+L["pro.pen.9"] = "9mm 穿透"
+L["pro.pen.12"] = "12mm 穿透"
+L["pro.pen.24"] = "24mm 穿透"
+L["pro.num.8"] = "开火发射 8 颗弹丸"
+
+L["pro.gsoe.stable"] = "瞄准开火动画稳定"
+L["con.gsoe.unstable"] = "瞄准开火动画不稳定"
+
+L["pro.gsoe.overdrive.1"] = "过热超过 100% 会进入过载"
+L["pro.gsoe.overdrive.2"] = "过载增加时伤害增加"
+L["pro.gsoe.overdrive.3"] = "过载超过 200% 时有几率点燃目标"
+
+L["con.gsoe.overdrive.1"] = "过载增加时散射增加"
+L["con.gsoe.overdrive.2"] = "过载增加时射速降低"
+L["con.gsoe.overdrive.3"] = "过载超过 200% 时用户会被烧伤"
+
+L["desc.gsoe.overdrive.1"] = "过载超过 300% 后设备损坏"
+L["desc.gsoe.overdrive.2"] = "警惕：绝不要让过载超过 500%！"
+
+L["pro.gsoe.dragonsbreath"] = "射出一圈燃烧目标的镁粉"
+L["con.gsoe.dragonsbreath"] = "镁粉受重力影响且射速低"
+L["con.gsoe.add_accuracymoa.30"] = "+30MOA 散射"
+
+L["ui.gsoe.cook"] = "弹药爆炸！丢失 %d 发"
+L["ui.gsoe.overdrive"] = "过载"
