cb05bc031bd131353a30a34deb9349c7ea69613b
thejaviertc <javier.toribiocouz@gmail.com>
2022-06-30 15:35:04 +0200

    Translated Gunsmith Offensive

---
+++ lua/arccw/shared/languages/go_es-es.lua
@@ -0,0 +1,23 @@
+L = {}
+
+L["pro.diver"] = "Dispara debajo del agua"
+L["pro.quickdraw"] = "Desenfunda/enfunda más rápidamente"
+L["pro.last"] = "Daño del último disparo"
+L["pro.ace"] = "Probabilidad de críticos hacia NPCs"
+L["pro.magcap"] = "Aumenta la capacidad del cargador"
+L["pro.refund"] = "Recarga una bala en el cargador por cada tiro en la cabeza"
+L["pro.runandgun"] = "Dispara mientras corres"
+L["pro.headshotdamage"] = "Triple de daño en la cabeza"
+L["pro.slowenemies"] = "Ralentiza a los enemigos al impactar"
+L["pro.invistracers"] = "Rastros invisibles"
+L["pro.fullauto"] = "Modo de Disparo Automático"
+
+L["con.magcap"] = "Reduce la capacidad del cargador"
+L["con.beam"] = "Láser visible"
+L["con.light"] = "Linterna visible"
+L["con.noscope"] = "Sin punto de mira"
+
+L["pro.ammo_blank"] = "!Inofensivo!"
+L["con.ammo_blank"] = "Inofensivo."
+
+L["pro.ignite"] = "Prende en fuego a los enemigos"
